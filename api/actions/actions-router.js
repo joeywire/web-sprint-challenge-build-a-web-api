a359291c94691bd2a7c7ed870d8e420d0171f926
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-04 10:19:20 -0600

    Fixed actions update/ put MVP complete

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -38,9 +38,8 @@ router.get('/:id', async (req, res) => {
 router.put('/:id', async (req, res) => {
     const { id } = req.params; 
     const { body } = req; 
-    console.log(body);
     try { 
-        const updatedAction = await Action.insert(id, body);
+        const updatedAction = await Action.update(id, body);
         res.status(200).json(updatedAction); 
     } catch (err) {
         res.status(500).json({ message: err.message });
