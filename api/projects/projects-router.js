f051403e9c17967ee24820020798bd4a67055e70
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-04 09:01:57 -0600

    Projects Router built - first endpoint working

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,18 @@
-// Write your "projects" router here!
+const express = require('express');
+const Project = require('./projects-model'); 
+
+const router=express.Router();
+
+
+//ENDPOINTS 
+router.get('/', async (req, res) => {
+    try { 
+        const projects = await Project.get();
+        res.status(200).json(projects);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+
+module.exports = router; 
--- api/server.js
+++ api/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
+const actionsRouter = require('./actions/actions-router'); 
+const projectsRouter = require('./projects/projects-router'); 
 
 const server = express();
 
 //Middleware _)_)_)_)_)_)_)_)_)_)_)_)_)_)_)_)_)
 server.use(express.json()); 
+
+//Routes 
+// server.use('/api/actions', actionsRouter); 
+server.use('/api/projects', projectsRouter); 
+
 
 //Catch All End Point 
 server.get('/', (req, res) => {
